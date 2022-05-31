9ea93a364087033b8c8495a518dceea03f168cee
knzheng <knzheng@student.ubc.ca>
2022-05-30 22:47:41 -0700

    finished navbar

---
+++ A2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import RecipeBody from './components/RecipeBody';
+function App() {
+  return (
+        <div className="body-div">
+          <RecipeBody />
+        </div>
+  );
+}
+
+export default App;
---
+++ A2/src/components/RecipeBody.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import InputForm from "./InputForm";
+export default function RecipeBody() {
+    return (
+        <div>
+            <Navbar />
+            <InputForm />
+            {/* <ReicipLists /> */}
+        </div>
+    );
+}
