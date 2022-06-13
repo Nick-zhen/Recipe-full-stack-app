964db2d8668ab190fa98dfc3f7d77d93e846187d
knzheng <knzheng@student.ubc.ca>
2022-06-13 15:04:48 -0700

    change to A3

--- A2/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import buttonOperation from './buttonOperation';
-
-const rootReducer = combineReducers({
-    buttonOperation
-});
-
-export default rootReducer;
