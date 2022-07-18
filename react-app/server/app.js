58914bf3a71d76940bff5a878d95b0c33b93457c
knzheng <knzheng@student.ubc.ca>
2022-07-17 21:44:50 -0700

    change folder name

---
+++ react-app/server/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
+const recipeRouter = require('./routes/recipes');
+const colors = require('colors');
+const app = express();
+
+//initialize DB connection
+const connectDB = require('./config/db');
+connectDB();
+// init middleware
+app.use(cors());
+app.use(logger('dev'));
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// homepage router
+app.use('/', indexRouter);
+// users Routes
+app.use('/users', userRouter);
+// recipes Routes
+app.use('/recipes', recipeRouter);
+
+module.exports = app;
---
+++ react-app/server/models/recipeModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+// create schema
+const recipeSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        name: {
+            type: String,
+            required: [true, 'Please add a name value'],
+        },
+        ingredients: {
+            type: String,
+            required: [true, 'Please add a ingredients value'],
+        },
+        steps: {
+            type: String,
+            required: [true, 'Please add a steps value'],
+        },
+        likes: {
+            type: Number
+        },
+        date: {
+            type: Date
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+recipeSchema.methods.speak = function speak() {
+    console.log(`I am a recipe named ${this.name}`);
+}
+
+// create model
+// The first argument is the singular name of the collection your model is for
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
